14f20254482b6cd3cbf0f7366175e916c964c819
dvl38 <101193264+dvl38@users.noreply.github.com>
2026-03-05 21:08:28 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:08:24.331Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-05T18:08:28.284Z
+Commit: Patch API endpoint error handling
