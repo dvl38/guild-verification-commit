5ddc56489c57f7705b3ef06d53c4046958de09c8
dvl38 <101193264+dvl38@users.noreply.github.com>
2026-03-05 21:06:03 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:06:01.903Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-05T18:06:03.331Z
+Commit: Fix memory leak in event handlers
